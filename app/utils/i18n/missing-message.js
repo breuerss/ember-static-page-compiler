1911fdabff2c3bb29dbafddd7d1c2e7bff78af46
Sebastian Breuers <sbreuers@gmail.com>
2018-05-06 20:59:20 +0200

    feature: hide missing translations

---
+++ app/utils/i18n/missing-message.js
@@ -0,0 +1,3 @@
+export default function i18nMissingMessage(locale, key, context) {
+    return key;
+}
---
+++ tests/unit/utils/i18n/missing-message-test.js
@@ -0,0 +1,10 @@
+import i18nMissingMessage from 'static-page-compiler/utils/i18n/missing-message';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | i18n/missing message');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = i18nMissingMessage();
+  assert.ok(result);
+});
