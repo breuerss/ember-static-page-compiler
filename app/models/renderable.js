3ea68224256146462d999c2fc8817e8141d79ef7
Sebastian Breuers <sbreuers@gmail.com>
2018-05-06 13:02:38 +0200

    feature: add possibility to convert renderable to JS object

--- app/models/renderable.js
+++ app/models/renderable.js
@@ -2,4 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Object.extend({
     template: '',
+    toNative() {
+        return Object.assign({}, this);
+    },
+    toJson() {
+        return JSON.stringify(this.toNative());
+    }
 });
