57ce4731f9a29c6145192172d9ef9bc5101577c1
Sebastian Breuers <sbreuers@gmail.com>
2018-05-07 20:07:06 +0200

    feature: add support for a selected model
    each of the models project, page, and content should be selectable.
    Depending on the selected model a parent page can be identified and a
    parent project.

--- app/services/project-manager.js
+++ app/services/project-manager.js
@@ -1,5 +1,46 @@
 import Ember from 'ember';
+import Project from '../models/project';
+import Page from '../models/page';
+import Content from '../models/content';
 
 export default Ember.Service.extend({
     projects: [],
+
+    selectedModel: null,
+    selectedPage: Ember.computed('selectedModel', function () {
+        const model = this.get('selectedModel');
+        return this.findClosestOfType(model, Page);
+    }),
+
+    selectedProject: Ember.computed('selectedModel', function () {
+        const model = this.get('selectedModel');
+        return this.findClosestOfType(model, Project);
+    }),
+
+    selectedContent: Ember.computed('selectedModel', function () {
+        const model = this.get('selectedModel');
+        return this.findClosestOfType(model, Content);
+    }),
+
+    findClosestOfType(model, type) {
+        if (model === null) {
+            return null;
+        }
+
+        if (this.is(model, type)) {
+            return model;
+        }
+
+        const parentModel = model.get('parentModel');
+        if (parentModel === undefined) {
+            return null;
+        }
+
+        return this.findClosestOfType(parentModel, type);
+    },
+
+    is (model, type) {
+        return type.prototype.isPrototypeOf(model);
+    },
+
 });
