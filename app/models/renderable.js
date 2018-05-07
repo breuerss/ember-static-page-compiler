e3771d805176231805749c6d03fdcd7501bb9246
Sebastian Breuers <sbreuers@gmail.com>
2018-05-07 23:13:56 +0200

    fix: ensure that input to JSON.stringify can be converted

--- app/models/renderable.js
+++ app/models/renderable.js
@@ -3,10 +3,27 @@ import Selectable from '../mixins/selectable-model';
 
 export default Ember.Object.extend(Selectable, {
     template: '',
+
     toNative() {
         return Object.assign({}, this);
     },
+
+    toData () {
+        let native = this.toNative();
+
+        return Object.keys(native).reduce((obj, key) => {
+            let value = native[key];
+            console.log(typeof value);
+            if ((typeof value).match(/(string|number|boolean)/)) {
+                obj[key] = value;
+            }
+
+            return obj;
+        }, {});
+    },
+
     toJson() {
-        return JSON.stringify(this.toNative());
-    }
+        return JSON.stringify(this.toData());
+    },
+
 });
