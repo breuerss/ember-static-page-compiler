0d2fae0cfafcc973c724430652437f5bb0feaaad
Sebastian Breuers <sbreuers@gmail.com>
2018-05-09 00:13:52 +0200

    feature: add support for rendering contents into template

---
+++ app/initializers/register-handlebars-helper.js
@@ -0,0 +1,17 @@
+import Handlebars from 'handlebars';
+export function initialize() {
+    Handlebars.registerHelper('render-contents', function (context, options) {
+        let rendered = context.reduce((rendered, model) => {
+            const template = Handlebars.compile(model.get('template'));
+            const data = model.toNative();
+            return rendered + template(data);
+        }, '');
+
+        return rendered;
+    });
+}
+
+export default {
+  name: 'register-handlebars-helper',
+  initialize
+};
