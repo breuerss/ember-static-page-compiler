699aadcc5dcad89576ec4edc8b00f987afddee5c
Sebastian Breuers <sbreuers@gmail.com>
2018-05-11 16:17:21 +0200

    feature: allow extension of template with placeholders

--- app/controllers/content/edit.js
+++ app/controllers/content/edit.js
@@ -3,4 +3,18 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     projectManager: Ember.inject.service(),
     contentModel: Ember.computed.alias('projectManager.selectedContent'),
+    additionalFields: Ember.computed('contentModel', 'contentModel.template', function () {
+        const template = this.get('contentModel.template');
+        let fields = [];
+        if (template) {
+            fields = template.match(/{{.*?}}/g) || [];
+            fields = fields.map(field => {
+                return field.replace('{{', '').replace('}}', '').trim();
+            });
+
+            fields = fields.filter(field => !field.match(/(title|content)/));
+        }
+
+        return fields;
+    }),
 });
---
+++ app/helpers/get.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export function get(params/*, hash*/) {
+    const model = params[0];
+    const key = params[1];
+    return model.get(key);;
+}
+
+export default Ember.Helper.helper(get);
