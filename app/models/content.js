6b699ef58415c31ad69defebf8359ef542133fcb
Sebastian Breuers <sbreuers@gmail.com>
2018-05-11 16:16:49 +0200

    feature: give content a default template

--- app/models/content.js
+++ app/models/content.js
@@ -3,4 +3,8 @@ import Renderable from './renderable';
 export default Renderable.extend({
     title: '',
     content: '',
+    template: [
+        '<h2>{{title}}</h2>',
+        '<p>{{content}}</p>',
+    ].join('')
 });
